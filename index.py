a10f27d81ce98ade976cb07aa1382a296c8c5994
Bruce Gordon <brucegne@gmail.com>
2023-04-05 19:54:29 +0000

    Dev Step

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import gspread
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import requests, json
 
-st.header = "welcome to my stuff"
-st.subheader = "Here we go...."
+st.set_page_config(page_title="Nifty Stuff",
+                    page_icon=":bar_chart:",
+                    layout="wide")
 
-
+st.title("Welcome to StreamLit")
