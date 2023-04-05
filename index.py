41cd0f74b7ad3613fb49886c7261d166197408d7
Bruce Gordon <brucegne@gmail.com>
2023-04-05 15:19:20 -0500

    Update index.py

--- index.py
+++ index.py
@@ -10,3 +10,13 @@ st.set_page_config(page_title="Nifty Stuff",
                     layout="wide")
 
 st.title("Welcome to StreamLit")
+
+work_url = 'https://socialpancakes-d1dad.firebaseio.com/bdata/Users.json'
+
+# requests.post(work_url, json={'name':'Gordon, Dana Marie', 'age': 8})
+
+results = requests.get(work_url)
+res = results.json()
+for rec in res:
+    st.write(res[rec]['name'],res[rec]['comments'])
+    
