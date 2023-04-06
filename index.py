d29ab3308f0d1f4219bccc95d2c0a5e17f39394e
Bruce Gordon <brucegne@gmail.com>
2023-04-06 02:58:27 -0500

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Nifty Stuff",
                     layout="wide")
 
 
-page_nav = st.sidebar.radio("Select Page",["Firebase","About Us"])
+page_nav = st.sidebar.radio("Select Page",["Firebase","About Us", "Images"])
 if page_nav == "Firebase":
 
     """
@@ -32,3 +32,23 @@ if page_nav == "About Us":
 
     st.write("Ain't this stuff neat")
 
+if page_nav == "Images":
+
+  col1, col2, col3 = st.columns([1,2,1])
+
+  with col1:
+      col1.header("This is on the left")
+      col1.subheader("This would be a little smaller")
+
+  with col2:
+      col2.header("This should be in the center")
+      my_upload = st.file_uploader("Upload a local photo")
+      my_image = st.camera_input("Take your picture", disabled=False)
+
+  with col3:
+      col3.header("This is hanging out on the right")
+      if my_image:
+          st.success("You got the picture")
+          st.image(my_image)
+      if my_upload:
+          st.image(my_upload)
