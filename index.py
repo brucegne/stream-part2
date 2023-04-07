45489cfc503bd2859aab1ab80a0b2c5f8ad34253
Bruce Gordon <brucegne@gmail.com>
2023-04-07 11:32:04 -0500

    Update index.py

--- index.py
+++ index.py
@@ -33,6 +33,13 @@ if page_nav == "Firebase":
         st.write(res[rec]['name'],res[rec]['comments'])
     uname = st.sidebar.selectbox("Choose User", rlist)
     if "Please"  not in uname:
+      user_rec = requests.get(user_url % (klist[uname]))
+      with st.form(key='edit_form'):
+        uemail = user_rec.json()['email]
+        ucomments = user_rec.json()['comments']
+        user_name = st.sidebar.text_input(value=uname)
+        user_email = st.sidebar.text_input(value=uemail)
+        user_comments = st.sidebar.text_input(value=ucomments)
         st.sidebar.write(uname)
         st.sidebar.write(klist[uname])
         user_rec = requests.get(user_url % (klist[uname]))
