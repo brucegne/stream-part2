cafa5b32854c9b6021d94c056abbdd1825942464
Bruce Gordon <brucegne@gmail.com>
2023-04-07 14:50:52 -0500

    Update index.py

--- index.py
+++ index.py
@@ -52,7 +52,7 @@ if page_nav == "Firebase":
               post_data['comments'] = user_comments
               res = requests.patch(post_url,json=post_data)
               st.sidebar.write("Form was processed")
-              st.experimental_rerun()
+              st.stop()
 
               
 if page_nav == "About Us":
