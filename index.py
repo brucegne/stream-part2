6ab5100b4058df3d63ba5de1632dc6b9d0731dfc
Bruce Gordon <brucegne@gmail.com>
2023-04-11 10:45:04 -0500

    Update index.py

--- index.py
+++ index.py
@@ -9,8 +9,28 @@ st.set_page_config(page_title="Nifty Stuff",
                     layout="wide")
 
 
-page_nav = st.sidebar.radio("Select Page",["Firebase","About Us", "Images"])
+page_nav = st.sidebar.radio("Select Page",["Add User","Firebase","About Us", "Images"])
 
+if page_nav = "Add User":
+
+	work_url = 'https://socialpancakes-d1dad.firebaseio.com/bdata/Users.json'
+	user_url = 'https://socialpancakes-d1dad.firebaseio.com/bdata/Users/%s.json'
+
+
+	form =  st.form(key='editform1234',clear_on_submit=True)
+	user_name = form.text_input('User Name', value='')
+	user_email = form.text_input('User Email', value='')
+	user_comments = form.text_input('Comments', value='')
+	usr_submit = form.form_submit_button(label="Save Changes")
+	if usr_submit:
+	    post_url = work_url
+	    post_data = {}
+	    post_data['name'] = user_name
+	    post_data['email'] = user_email
+	    post_data['comments'] = user_comments
+	    res = requests.post(post_url,json=post_data)
+	    page_nav = "Firebase"
+	
 if page_nav == "Firebase":
 
 	st.title("Welcome to StreamLit")
