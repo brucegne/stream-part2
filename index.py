02f76c496e92f23b20b8c8c328fb6e7c5591e342
Bruce Gordon <brucegne@gmail.com>
2023-04-11 17:44:44 -0500

    Update index.py

--- index.py
+++ index.py
@@ -21,7 +21,7 @@ if page_nav == "Add User":
 	form =  st.form(key='editform1234',clear_on_submit=True)
 	user_name = form.text_input('User Name', value='')
 	user_email = form.text_input('User Email', value='')
-	user_comments = form.text_input('Comments', value='')
+	user_comments = form.text_area('Comments', value='')
 	usr_submit = form.form_submit_button(label="Save Changes")
 	if usr_submit:
 	    post_url = work_url
