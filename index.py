a827b3c43ce5faf3b77d7a3c931f9961b280ae3b
Bruce Gordon <brucegne@gmail.com>
2023-04-12 04:50:16 -0500

    Update index.py

--- index.py
+++ index.py
@@ -46,7 +46,8 @@ if page_nav == "Edit User":
 	srch_submit = srch_form.form_submit_button(label="Search")
 	if srch_submit:
 		user_rec = requests.get(user_url % (rec_key))
-		st.write(user_rec.status)
+		
+		st.write(rec_key, user_rec)
 		form =  st.form(key='editform1234',clear_on_submit=True)
 		uname = user_rec.json()['name']
 		uemail = user_rec.json()['email']
