7f20006cfca056d379b1b9e257c09d5711be9297
Bruce Gordon <brucegne@gmail.com>
2023-04-25 15:29:43 -0500

    Image Display

--- pages/01_Comps.py
+++ pages/01_Comps.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import os, json, time
+from PIL import Image
 
 st.write(st.session_state)
 
 def save_uploadedfile(uploadedfile):
      with open(os.path.join("static",uploadedfile.name),"wb") as f:
          f.write(uploadedfile.getbuffer())
-     return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+     return st.success("Saved File:{} to /static".format(uploadedfile.name))
 
 image_file = st.file_uploader("Upload An Image",type=['png','jpeg','jpg'])
 if image_file is not None:
     file_details = {"FileName":image_file.name,"FileType":image_file.type}
     st.write(file_details)
     save_uploadedfile(image_file)
+
+st.markdown(
+        '<img src="./app/static/gordon43.jpg" height="333" style="border: 5px solid orange">',
+        unsafe_allow_html=True,
+    )
