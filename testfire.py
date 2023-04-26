dac99469b1c394bbace133d9434d48f6b9b96d2b
Bruce Gordon <brucegne@gmail.com>
2023-04-26 14:44:42 -0500

    Firestore

---
+++ testfire.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import json
+
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="socialpancakes-d1dad")
