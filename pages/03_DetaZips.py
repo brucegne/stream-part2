a4faf613ed970a014bbfe24747564458040467cc
Bruce Gordon <brucegne@gmail.com>
2023-04-28 09:57:37 -0500

    Finally fixed the json

--- pages/03_DetaZips.py
+++ pages/03_DetaZips.py
@@ -6,11 +6,11 @@ import json
 deta = Deta("b0fhjqxu_fG4y33DEMaK8qWfMGABUSbn8cGFNxXhC")
 db = deta.Base("Zipcode")
 hdr = st.container()
-city=st.text_input("Enter city")
-county=st.text_input("Or enter county")
-state =st.text_input("Enter 2 character state")
+city = st.text_input("Enter city")
+county = st.text_input("Or enter county")
+state = st.text_input("Enter 2 character state")
 goLook = st.button("Search")
-if goLook: 
+if goLook:
     qryString={}
     if city.strip() == "":
         qryString["county?contains"] = county.title()
@@ -19,8 +19,12 @@ if goLook:
     if state.strip() != "":
         qryString["state"] = state.upper()
     db_content = db.fetch(qryString).items
-    
+
     st.dataframe(db_content)
-    hdr.write("Total locations found :"+str(len(db_content)))
-    st.markdown("<h1 style='text-align: center; color: grey;'>Big headline</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: center; color: red;'>Smaller headline in black </h2>", unsafe_allow_html=True)
+    if len(db_content) > 0:
+        hdr.write("Total locations found :"+str(len(db_content)))
+        st.markdown("<h1 style='text-align: center; color: grey;'>Big headline</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: red;'>Smaller headline in black </h2>", unsafe_allow_html=True)
+    city = ""
+    county = ""
+    state = ""
